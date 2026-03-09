a13a2b7a80d5ef93f5ec179752ce54905b15f784
Goal651 <bugiriwilson651@gmail.com>
2026-03-09 07:59:15 +0200

    feat: Implement regression model training and add an endpoint for regression analysis.

--- predictor/urls.py
+++ predictor/urls.py
@@ -3,4 +3,6 @@ from predictor import views
 
 urlpatterns = [
     path("data_exploration", views.data_exploration_view, name="data_exploration"),
+    path("regression_analysis", views.regression_analysis,
+         name="regression_analysis"),
 ]
